57df72f8801bf6fd3516483290751e191b68e6aa
mrmarkolinus <72712091+mrmarkolinus@users.noreply.github.com>
2023-12-18 20:44:41 +0100

    test commit

--- python/test_rust_lib.py
+++ python/test_rust_lib.py
@@ -1,6 +1,6 @@
 import rnbt as rnbt # This should match the name of your .so or .pyd file.
 #import logging
-
+#TEST TEST
 #logging.basicConfig(level=logging.INFO)
 #mc_binary = rnbt.load_binary('tests/resources/test_world/r.-1.0.mca')
 
